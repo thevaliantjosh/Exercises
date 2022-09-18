46d09b09104fbbd9e3e2a402cc22c1f9ef3f8bea
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-09-18 15:35:59 -0500

    Made an array of Objects for games

--- js/understanding_the_DOM.js
+++ js/understanding_the_DOM.js
@@ -2,13 +2,35 @@
 // alert("Welcome to Understanding the DOM");
 
 /*This will output all the child nodes in the section element*/
-let myNodeList = document.body.childNodes[1].childNodes;
-    for (let i = 0; i < myNodeList.length; i++) {
-        document.write(myNodeList[i] + " <br>");
-    }
+// let myNodeList = document.body.childNodes[1].childNodes;
+//     for (let i = 0; i < myNodeList.length; i++) {
+//         document.write(myNodeList[i] + " <br>");
+//     }
+//
 
-function boldFirstAndLastNav() {
-        document.body.childNodes[1].firstChild.style.fontWeight = "bold";
-        document.body.childNodes[1].lastChild.style.fontWeight = "bold";
-}
-    boldFirstAndLastNav();
+    /*Want to hover over a Nav item and Make it change color and grow*/
+
+
+// let theLinks = document.querySelectorAll("a");
+// theLinks.addEventListener = ("mouseover", function transformLinks() {
+//     theLinks.querySelectorAll("a");
+//     hoverOver.style.fontSize = "3rem";
+//     hoverOver.style.color = "green";
+// }
+
+const games = [
+    {Name: "Oblivion", Genre: "RPG"},
+    {Name: "Morrowind", Genre: "RPG"},
+    {Name: "Halo Wars 2", Genre: "Strategy"},
+    {Name: "Civilization VI", Genre: "Strategy"},
+    {Name: "Halo Infinite", Genre: "First Person Shooters"},
+    {Name: "Overwatch", Genre: "First Person Shooters"},
+    {Name: "Red Dead Redemption 2", Genre: "Action & Adventure"},
+    {Name: "No Man's Sky", Genre: "Action & Adventure"},
+    {Name: "Darkest Dungeon", Genre: "Roguelike"},
+    {Name: "Hades", Genre: "Roguelike"},
+    {Name: "Farming Simulator 22", Genre: "Simulation"},
+    {Name: "The Sims 4", Genre: "Simulation"},
+    {Name: "Slay the Spire", Genre: "Card & Board"},
+    {Name: "Gwent", Genre: "Card & Board"},
+];
