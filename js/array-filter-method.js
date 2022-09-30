4a34ee487b9b630fc330644759c68fe3e65cdd70
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-09-30 12:05:56 -0500

    Logged the data from people, and extracted the data forEach Character in a console.log

--- js/array-filter-method.js
+++ js/array-filter-method.js
@@ -1,2 +1,13 @@
 "use strict"
 console.log("Welcome to the array Filter Method");
+
+$(function (){
+    $.ajax("https://swapi.dev/api/people").done(function(data){
+        console.log(data);
+       data.results.forEach(function(character){
+           console.log(character)
+
+       })
+    }
+)
+});//End of
