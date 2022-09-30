66e2a756c109051e11722385e54381aec0171962
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-09-30 13:23:46 -0500

    Added Darth Vader and Owen Lars to the page, filtered by mass above 100

--- js/array-filter-method.js
+++ js/array-filter-method.js
@@ -1,13 +1,25 @@
 "use strict"
 console.log("Welcome to the array Filter Method");
 
+
+
 $(function (){
+    let output = "";
     $.ajax("https://swapi.dev/api/people").done(function(data){
         console.log(data);
-       data.results.forEach(function(character){
-           console.log(character)
+        const massAbove100 = data.results.filter(char => char.mass > 100);
+        console.log(massAbove100);
 
-       })
-    }
-)
-});//End of
+        massAbove100.forEach(({name, mass }, index) => {
+            output += `<li>${index + 1}: ${name} has a mass of: ${mass}</li>`
+            $("#mass100").html(output)
+        });//End of the ForEach loop which is an Array of Objects with Characters mass above 100.
+
+
+        data.results.forEach(function(character){
+           console.log(character);
+           console.log(character.mass)
+
+       });//End of Character data forEach
+    });//End of Ajax.done
+});//End of document.ready
